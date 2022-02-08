b25779cd5267a0660a49ce4f94821a3ff4027d07
Alex Woods <alexanderwoods2019@gmail.com>
2022-02-08 13:36:24 -0500

    Completed introducing methods tutorial

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    #region Variables
+    // Variables
+    #endregion
+
+    #region Unity Methods
+    // Built in Unity Methods
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, .1f);
+    }
+
+    #endregion
+
+    #region Private Methods
+    // Private Methods.
+    #endregion
+
+    #region Public Methods
+    // Public Methods
+    #endregion
+}
