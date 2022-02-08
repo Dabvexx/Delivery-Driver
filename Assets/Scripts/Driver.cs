a3a1a3825fcca6197669c7e8c0c196d1994dce85
Alex Woods <alexanderwoods2019@gmail.com>
2022-02-08 15:09:12 -0500

    Transform.Translate(). Added HandleInput() Method

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,33 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
     #region Variables
+
     // Variables
-    #endregion
+    [SerializeField]
+    [Range(.1f, 500f)]
+    private float turnSpeed;
+
+    [SerializeField]
+    [Range(.1f, 50f)]
+    private float moveSpeed;
+
+    #endregion Variables
 
     #region Unity Methods
+
     // Built in Unity Methods
 
-    void Start()
+    private void Update()
     {
-
+        HandleInput();
     }
 
-    void Update()
-    {
-        transform.Rotate(0, 0, .1f);
-    }
-
-    #endregion
+    #endregion Unity Methods
 
     #region Private Methods
-    // Private Methods.
-    #endregion
 
-    #region Public Methods
-    // Public Methods
-    #endregion
+    // Private Methods.
+    private void HandleInput()
+    {
+        var inputY = Input.GetAxis("Vertical");
+        var inputX = Input.GetAxis("Horizontal");
+
+        // Handle Rotation.
+        transform.Rotate(0, 0, -inputX * turnSpeed * Time.deltaTime);
+
+        // Handle Acceleration.
+        transform.Translate(0,  inputY * moveSpeed * Time.deltaTime, 0);
+    }
+
+    #endregion Private Methods
 }
